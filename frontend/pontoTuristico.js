67b5dd0df120f36672bcaf33339390e49372232b
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-04-05 23:01:32 -0300

    refactor - corrigindo endereçamento de tela pontoTuristico

--- frontend/pontoTuristico.js
+++ frontend/pontoTuristico.js
@@ -1,15 +1,12 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    const button = document.getElementById("pontos")
+    const button = document.getElementById('pontos')
     if(button){
         button.addEventListener('load', pontos);
     }
 })
 
 async function pontos() {
-    const nome = document.getElementById("#nome");
-    const local = document.getElementById("#local");
-    const informacoes = document.getElementById("#informacoes");
-    const avaliacoes = document.getElementById("#avaliacoes");
+
     
     try {
         const response = await fetch("http://localhost:8080/pontoTuristico/pontos", {
@@ -17,22 +14,18 @@ async function pontos() {
         headers: {
             "content-type": "application/json"
 
-        },
-        body: JSON.stringify({
-            nome: nome,
-            local: local,
-            informacoes: informacoes,
-            avaliacoes: avaliacoes
-        })
-    });
-        const data = await response.text;
-
-        if(response.ok){
-            console.log("listando pontos com sucesso!");
-        }else{
-            alert("ERRO: " + data);
         }
         
+    });
+        if(response.ok){
+            console.log("Tá tudo ok");
+        }else{
+            throw new Error("Erro ao listar os pontos turisticos: " + response.statusText);
+        }
+
+        const listaPontos = await response.json;
+        console.log(listaPontos);
+
     } catch (error) {
         console.error("Erro na listagem de pontos turisticos: " + error);
         alert(error);
