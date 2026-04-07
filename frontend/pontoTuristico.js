62d0bfd08592b7e141c034cd47ae108dfd6ccdef
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-04-07 20:58:21 -0300

    feat - conectando front com o back e recebendo a lista de ponto turisticos na tela

--- frontend/pontoTuristico.js
+++ frontend/pontoTuristico.js
@@ -1,34 +1,50 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-    const button = document.getElementById('pontos')
-    if(button){
-        button.addEventListener('load', pontos);
-    }
-})
+document.addEventListener('DOMContentLoaded', function() {
+    pontos();
+});
 
 async function pontos() {
 
-    
     try {
         const response = await fetch("http://localhost:8080/pontoTuristico/pontos", {
-        method: "GET",
+        method: 'GET',
         headers: {
-            "content-type": "application/json"
+            'content-type': 'application/json'
 
         }
         
     });
         if(response.ok){
             console.log("Tá tudo ok");
+        
         }else{
             throw new Error("Erro ao listar os pontos turisticos: " + response.statusText);
         }
 
-        const listaPontos = await response.json;
+        const listaPontos = await response.json();
         console.log(listaPontos);
+        exibirPontos(listaPontos);
 
     } catch (error) {
         console.error("Erro na listagem de pontos turisticos: " + error);
         alert(error);
     }
-    
+}
+function exibirPontos(dados) {
+    const tbody = document.getElementById('lista-pontos');
+    tbody.innerHTML = "";
+    const pontos = dados.content;
+
+
+
+    pontos.forEach(ponto => {
+        const linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>${ponto.nome}</td>
+            <td>${ponto.local}</td>
+            <td>${ponto.informacoes}</td>
+            <td>${ponto.avaliacoes || '0'} ⭐</td>
+        `;
+        tbody.appendChild(linha);
+    });
+
 }
