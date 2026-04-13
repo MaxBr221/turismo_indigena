a681de2a52d967e2c34d8257a8424599295a19d4
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-04-12 22:17:29 -0300

    feat - adicionando implementação de tela de restaurante

--- frontend/restaurante.js
+++ frontend/restaurante.js
@@ -1,5 +1,53 @@
-async function restaurantes() {
+document.addEventListener('DOMContentLoaded', function(){
+        restaurantes();
+    });
 
+async function restaurantes() {
     
+    try {
+        const response = await fetch("http://localhost:8080/restaurantes/restaurantePaginacao",{
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+
+        if(response.ok){
+            console.log("busca com sucesso!");
+        }else{
+            console.log("Erro ao buscar");
+            alert("Erro ao fazer a busca");
+        }
+
+        const salvos = response.json;
+        console.log("Guardando restaurantes");
+        listarRestaurantes(salvos);
+
+
+    } catch (error) {
+        alert("erro no sistema: " + error);
+    }
+    
+}
+
+function listarRestaurantes(dados){
+    const tbody = document.getElementById('listarRestaurantes');
+    tbody.innerHTML = "";
+    const restaurantes = dados.content;
+
+
+    restaurantes.array.forEach(r => {
+        const linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>Nome</td>
+            <td>Descrição</td>
+            <td>Localização</td>
+            <td>Horário</td>
+            <td>Telefone</td>
+            <td>Rede Social</td>
+            <td>Avaliações</td>
+        `
+        tbody.appendChild(linha);
+    });
     
 }
